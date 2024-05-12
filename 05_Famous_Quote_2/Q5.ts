b7651dfdf12b1b17129236b41e1801b4a2e80ada
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 20:22:59 +0500

    Add files via upload

---
+++ 05_Famous_Quote_2/Q5.ts
@@ -0,0 +1,8 @@
+/*Q5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a
+ variable called famous_person. Then compose your message and store it in a new variable
+ called message. Print your message.*/
+
+ let famous_person: string ="Nelson Mandela"
+ let message: string = "“Education is the most powerful weapon which you can use to change the world.”"
+ 
+ console.log(`${famous_person} once said, ${message}`);
