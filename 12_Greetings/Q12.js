d7285978a44533f51aa10ab6ef4c2cd437b5363f
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 22:02:04 +0500

    Add files via upload

---
+++ 12_Greetings/Q12.js
@@ -0,0 +1,10 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing
+each person's name, print a message to them. The text of each message should be the same,
+but each meassage should be personalized with the person's name.*/
+let names = ["Maha", "Saima", "Imran", "Ali"];
+let message = "How Are You Dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
+export {};
