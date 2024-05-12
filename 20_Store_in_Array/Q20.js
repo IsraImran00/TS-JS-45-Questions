cd9a9c34171c99b6872b201486309da53440f51a
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 23:41:03 +0500

    Add files via upload

---
+++ 20_Store_in_Array/Q20.js
@@ -0,0 +1,12 @@
+/*Think of something you could store in a array. For example, you
+ could make a list of mountains, rivers, countries, cities,
+ languages, or anything else you’d like. Write a program that
+ creates a list containing these items.*/
+var mountains = [
+    "Nanga Parbat", "K-2", "Karakoram", "Himalaya", "Margalla Hills", "Hindu Kush", "Broad Peak"
+];
+mountains.map(function (item) { return console.log(item); });
+var proglanguages = [
+    "HTML", "CSS", "javaScript/TypeScript", "php"
+];
+proglanguages.map(function (item) { return console.log(item); });
---
+++ 20_Store_in_Array/Q20.ts
@@ -0,0 +1,12 @@
+/*Think of something you could store in a array. For example, you
+ could make a list of mountains, rivers, countries, cities, 
+ languages, or anything else you’d like. Write a program that
+ creates a list containing these items.*/
+
+ let mountains: string[] =[
+    "Nanga Parbat", "K-2", "Karakoram", "Himalaya", "Margalla Hills", "Hindu Kush", "Broad Peak"];
+     mountains.map((item) => console.log(item));
+
+ let proglanguages:string[] =[
+    "HTML", "CSS","javaScript/TypeScript","php"];
+     proglanguages.map((item) => console.log(item));
