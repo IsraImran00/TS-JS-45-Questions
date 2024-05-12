513a9b6c009294745d6ec82c7af7cd1ff7c2895a
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 22:07:29 +0500

    Add files via upload

---
+++ 13_Your_Own_Array/Q13.js
@@ -0,0 +1,7 @@
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a
+ car, and make a list that stores several examples. Use your list to print a series of
+ statements about these items, such as “I would like to own a Honda motorcycle.”
+ Define an array called 'favoriteTransportation' with car examples*/
+let vehicles = ["Ford Mustang", "Toyota Corolla", "Toyota Hilux", "Audi e-tron GT", "Honda City", "BMW M5"];
+vehicles.map((items) => console.log(`I would like to own a ${items}`));
+export {};
