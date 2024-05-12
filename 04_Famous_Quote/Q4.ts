80eee598001ec257d4e9c7e8e5beb1f96305ca2d
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 20:19:26 +0500

    Add files via upload

---
+++ 04_Famous_Quote/Q4.ts
@@ -0,0 +1,6 @@
+/*Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+*/
+
+let quote = "Quaid-e-Azam once said";
+console.log(`${quote}, “I do not believe in taking the right decision, I take a decision and make it right.”`);
