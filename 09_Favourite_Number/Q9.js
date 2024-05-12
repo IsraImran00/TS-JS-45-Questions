be767acf685b928be151baa7ffcf943c1e14bada
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 20:43:59 +0500

    Add files via upload

---
+++ 09_Favourite_Number/Q9.js
@@ -0,0 +1,5 @@
+/* Favourite Number: Store your favourite number in a variable. Then, using that variable
+ create a message that reveals your favourite number. Print that message.*/
+let num = 3011;
+console.log(`My Favourite Number is ${num}`);
+export {};
---
+++ 09_Favourite_Number/Q9.ts
@@ -0,0 +1,6 @@
+/* Favourite Number: Store your favourite number in a variable. Then, using that variable
+ create a message that reveals your favourite number. Print that message.*/
+
+ let num:number = 3011;
+
+ console.log(`My Favourite Number is ${num}`);
