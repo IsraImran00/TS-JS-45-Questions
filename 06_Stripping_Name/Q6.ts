bbbc068db1746d4e41e174119b7222a6ad442334
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-12 20:26:32 +0500

    Add files via upload

---
+++ 06_Stripping_Name/Q6.ts
@@ -0,0 +1,15 @@
+/*Q6. Stripping Names: Store a person’s name, and include some whitespace characters at the
+beginning and end of the name. Make sure you use each character combination, "\t" and "\n"
+,at least once. Print the name once, so the whitespace around the name is displayed. Then
+print the name after striping the white spaces.*/
+
+let name1: string = "Isra\tImran";
+let name2: string = "Isra\nImran";
+console.log(name1)
+console.log(name2)
+
+console.log("mynameisIsra");
+console.log("my\nname\nis\nIsra");  //used for  next lines.
+console.log("I\tAm\tThe\tStudent\tOf\tGovernor\tSindh\tInitiative"); //used for spaces b/w words.
+
+
