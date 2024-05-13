358db21b53f71c1d167765631746de6084fe93a6
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-13 20:59:13 +0500

    Add files via upload

---
+++ 23_Conditional_Tests/Q23.ts
@@ -0,0 +1,77 @@
+/* 23. Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for 
+ the results of each test. Your code should look something like 
+ this:
+ let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand 
+why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to
+ True and another 5 tests evaluate to False.*/
+
+// 1st true condition
+
+ let favCountry = 'Turkey';
+ console.log("Is favCountry == 'Turkey'? I predict True.")
+ console.log(favCountry == 'Turkey');
+
+// 2nd true condition
+
+ let favCar = 'Audi';
+ console.log("Is favCar == 'Audi'? I predict True.")
+ console.log(favCar == 'Audi');
+
+// 3rd true condition
+
+ let favNum = '22';
+ console.log("Is favNum == '22'? I predict True.")
+ console.log(favNum == '22');
+
+// 4th true condition
+
+ let favCoffee = 'Iced coffee';
+ console.log("Is favCoffee == 'Iced coffee'? I predict True.")
+ console.log(favCoffee == 'Iced coffee');
+
+// 5th true condition
+
+ let favFruit = 'Strawberry';
+ console.log("Is favFruit == 'Strawberry'? I predict True.")
+ console.log(favFruit == 'Strawberry');
+
+//  1st false condition
+
+ let favWeather = 'Winter';
+ console.log("Is favWeather == 'Summer'? I predict false.")
+ console.log(favWeather == 'Summer');
+
+//  2nd false condition
+
+let favMobile = 'Iphone';
+console.log("Is favMobile == 'Samsung'? I predict false.")
+console.log(favMobile == 'Samsung');
+
+// 3rd false condition
+
+let favCity = 'Islamabad';
+console.log("Is favCity == 'Lahore'? I predict false.")
+console.log(favCity == 'Lahore');
+
+// 4th false condition
+
+let favComputer = 'Dell';
+console.log("Is favComputer == 'Lenovo'? I predict false.")
+console.log(favComputer == 'Lenovo');
+
+// 5th false condition
+
+let favDay = 'Saturday';
+console.log("Is favDay == 'Monday'? I predict false.")
+console.log(favDay == 'Monday')
+
+
+
+
+
+
