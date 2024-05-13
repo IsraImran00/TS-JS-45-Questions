c0cda82cd9c1155fa7c31af0387d416776c4675d
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-13 21:05:35 +0500

    Add files via upload

---
+++ 24_More_Conditional_Tests/Q24.ts
@@ -0,0 +1,92 @@
+/*24. More Conditional Tests: You don’t have to limit the number
+ of tests you create to 10. If you want to try more comparisons, 
+ write more tests. Have at least one True and one False result 
+ for each of the following:
+// • Tests for equality and inequality with strings
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater
+ than and less than, greater than or equal to, and less than or 
+ equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array*/
+
+
+// 1. Tests for equality and inequality with strings
+//  Comparison Operators === , !==
+
+const country1 : string = "Pakistan";
+const country2 : string = "Saudia Arabia";
+
+console.log (country1 === country2);
+console.log (country1 !== country2);
+
+// 2. Tests using the lower case function
+
+const mixedCaseString: string = "Hello Isra";
+console.log(mixedCaseString.toLowerCase() === "hello isra")
+console.log(mixedCaseString.toLowerCase() === "helloisra")
+
+// 3. Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+const num1: number = 22;
+const num2: number = 30;
+
+// Equality 
+console.log(num1 === num2);
+
+// Inequality 
+console.log(num1 !== num2); 
+
+// Greater Than 
+console.log(num1 > num2); 
+
+// Less Than 
+console.log(num1 < num2); 
+
+// Greater Than or Equal To 
+console.log(num1 >= num2); 
+
+// Less Than or Equal To 
+console.log(num1 <= num2);
+
+// 4. Tests using "and" and "or" operators
+
+const Condition1: boolean = true;
+const Condition2: boolean = false;
+
+console.log(Condition1 && Condition2);
+console.log(Condition1 || Condition2);
+
+// 5. Test whether an item is in a array
+
+const Weather: string[] =["Summer", "Winter", "Autumn", "Spring"];
+console.log("Item in array test:", Weather.includes("Winter"));
+
+// 6. Test whether an item is not in a array
+console.log("Item not in array test:", Weather.includes("Rainy"));
+
+
+
+
+
+
+
+
+
+
+  
+
+// let Fruits = ["Apple", "Bnanana", "Cherry", "Stawberry", "Mango"]
+// console.log('Apple' === Fruits[0], "Apple is in Friut array/list.\n");
+
+// let array: string[] = ["Apple", "Banana", "Orange", "Litchi"];
+// console.log("Item in array test:", array.includes("Orange"));
+
+
+
+
+
+
+
+
